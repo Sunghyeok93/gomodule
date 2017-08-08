c93bd34bf716c187d7feb3aec3f36cbc76915206
gwangho0510 <lgh2802@naver.com>
2017-08-08 05:27:23 +0000

    complete

--- go-ssh.go
+++ go-ssh.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"github.com/tmc/scp"
 	"golang.org/x/crypto/ssh"
+	"io/ioutil"
 	"log"
 	"os"
 	"sync"
-    "io/ioutil"
 )
 
 func Make_Config(id string, passwd string) *ssh.ClientConfig {
@@ -22,6 +22,33 @@ func Make_Config(id string, passwd string) *ssh.ClientConfig {
 	return config
 }
 
+type Opt struct {
+	opt_A int
+	opt_M int
+	opt_C int
+	opt_S int
+}
+
+func check_opt() Opt {
+	var opts Opt
+	opts.opt_A = 0
+	opts.opt_M = 0
+	opts.opt_S = 0
+	opts.opt_C = 0
+	for i := 0; i < len(os.Args); i++ {
+		if os.Args[i] == "-a" {
+			opts.opt_A = i + 1
+		} else if os.Args[i] == "-m" {
+			opts.opt_M = i + 1
+		} else if os.Args[i] == "-c" {
+			opts.opt_C = i + 1
+		} else if os.Args[i] == "-s" {
+			opts.opt_S = i + 1
+		}
+	}
+	return opts
+}
+
 type Config struct {
 	Addr   string
 	Role   string
@@ -30,6 +57,7 @@ type Config struct {
 }
 
 func main() {
+	check := check_opt()
 	opt := os.Args[1]
 	file, err := ioutil.ReadFile(os.Args[2])
 	if err != nil {
@@ -40,33 +68,66 @@ func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < len(data); i++ {
 		wg.Add(1)
-		go func(ip, port, passwd string) {
+		go func(ip, port, passwd, role string) {
 			defer wg.Done()
 			addr := ip + ":" + port
 			config := Make_Config("root", passwd)
-
 			client, err := ssh.Dial("tcp", addr, config)
 			if err != nil {
 				log.Fatal("Failed to dial: ", err)
 			}
 			if opt == "-sh" {
-				session, err := client.NewSession()
-				defer session.Close()
-				if err != nil {
-					log.Fatal("Failed to create session: ", err)
+				if check.opt_C != 0 {
+					session, err := client.NewSession()
+					defer session.Close()
+					if err != nil {
+						log.Fatal("Failed to create session: ", err)
+					}
+					cmd := os.Args[check.opt_C]
+					if err := session.Run(cmd); err != nil {
+						log.Fatal("Failed to run: " + err.Error())
+					}
 				}
-				cmd := os.Args[3]
-				if err := session.Run(cmd); err != nil {
-					log.Fatal("Failed to run: " + err.Error())
+				if (check.opt_A != 0) && (role == "agent") {
+					session, err := client.NewSession()
+					if err != nil {
+						log.Fatal("Failed to create session: ", err)
+					}
+					cmd := os.Args[check.opt_A]
+					if err := session.Run(cmd); err != nil {
+						log.Fatal("Failed to run " + err.Error())
+					}
+				} else if (check.opt_M != 0) && (role == "master") {
+					session, err := client.NewSession()
+					if err != nil {
+						log.Fatal("Failed to create session: ", err)
+					}
+					cmd := os.Args[check.opt_M]
+					if err := session.Run(cmd); err != nil {
+						log.Fatal("Failed to run " + err.Error())
+					}
+				} else if (check.opt_S != 0) && (role == "storage") {
+					session, err := client.NewSession()
+					if err != nil {
+						log.Fatal("Failed to create session: ", err)
+					}
+					cmd := os.Args[check.opt_S]
+					if err := session.Run(cmd); err != nil {
+						log.Fatal("Failed to run " + err.Error())
+					}
 				}
 			} else if opt == "-cp" {
+				if (check.opt_A | check.opt_M) != 0 {
+					fmt.Println("'-a' or '-m' can be used for '-sh' option")
+					return
+				}
 				desPath := os.Args[len(os.Args)-1]
 				var cp_wg sync.WaitGroup
 				for j := 3; j < len(os.Args)-1; j++ {
 					cp_wg.Add(1)
 					go func(filePath, desPath string, client *ssh.Client) {
 						session, err := client.NewSession()
-                            defer session.Close()
+						defer session.Close()
 						if err != nil {
 							log.Fatal("Failed to create session: ", err)
 						}
@@ -79,7 +140,7 @@ func main() {
 				}
 				cp_wg.Wait()
 			}
-		}(data[i].Addr, data[i].Port, data[i].Passwd)
+		}(data[i].Addr, data[i].Port, data[i].Passwd, data[i].Role)
 	}
 	wg.Wait()
 	fmt.Println("Complete")
