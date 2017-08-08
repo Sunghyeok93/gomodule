e4a5495806798f0bc8a46a1a785e070f8c2cdcfd
gwangho0510 <lgh2802@naver.com>
2017-08-08 01:25:39 +0000

    multi-file-scp possible not json

--- go-ssh.go
+++ go-ssh.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/tmc/scp"
 	"golang.org/x/crypto/ssh"
 	"log"
 	"os"
 	"sync"
+    "github.com/tmc/scp"
 )
 
 func Make_Config(id string, passwd string) *ssh.ClientConfig {
@@ -19,7 +19,6 @@ func Make_Config(id string, passwd string) *ssh.ClientConfig {
 	}
 	return config
 }
-
 func main() {
 	opt := os.Args[1]
 	addr := os.Args[2] + ":" + os.Args[3]
@@ -37,32 +36,35 @@ func main() {
 		log.Fatal("Failed to create session: ", err)
 	}
 	if opt == "-sh" {
-        var cmd string
-        for i := 6; i<len(os.Args); i++{
-            cmd = cmd + " " + os.Args[i]
-        }
-        fmt.Println(cmd)
+		var cmd string
+		for i := 6; i < len(os.Args); i++ {
+			cmd = cmd + " " + os.Args[i]
+		}
 		wg.Add(1)
 		go func(session *ssh.Session, cmd string) {
 			defer wg.Done()
-				defer session.Close()
+			defer session.Close()
 			if err := session.Run(cmd); err != nil {
 				log.Fatal("Failed to run: " + err.Error())
 			}
 		}(session, cmd)
 	} else if opt == "-cp" {
-		filePath := os.Args[6]
-		desPath := os.Args[7]
-		wg.Add(1)
-		go func(filePath string, desPath string, session *ssh.Session) {
-			defer session.Close()
-			defer wg.Done()
-			err := scp.CopyPath(filePath, desPath, session)
-			if err != nil {
-				log.Fatal("Failed to scp: " + err.Error())
-			}
-		}(filePath, desPath, session)
-	}else{
+		desPath := os.Args[len(os.Args)-1]
+		for i := 6; i < len(os.Args)-1; i++ {
+			wg.Add(1)
+			go func(filePath, desPath string, client *ssh.Client) {
+				session, err := client.NewSession()
+				if err != nil {
+					log.Fatal("Failed to create session: ", err)
+				}
+				defer wg.Done()
+				err = scp.CopyPath(filePath, desPath, session)
+				if err != nil {
+					log.Fatal("Failed to scp: " + err.Error())
+				}
+			}(os.Args[i], desPath, client)
+		}
+	} else {
 		fmt.Println("Usage : <-sh|-cp> ADDR ID PASSWD <CMD|SOURCE FILE> <|DESTINATION FILE>")
 	}
 	wg.Wait()
