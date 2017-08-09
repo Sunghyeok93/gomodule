505e765c57f05fbfff10717c7bf3c45656944436
gwangho0510 <lgh2802@naver.com>
2017-08-09 05:35:54 +0000

    go-ssh_v1

--- go-ssh.go
+++ go-ssh.go
@@ -8,8 +8,56 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"sync"
 )
+
+type Opt struct {
+	opt_A [2]int
+	opt_M [2]int
+	opt_C [2]int
+	opt_S [2]int
+}
+
+type Config struct {
+	Addr   string
+	Role   string
+	Port   string
+	Passwd string
+}
+
+func endpoint(start int, arr []string) int {
+	for i := start + 1; i < len(arr); i++ {
+		if strings.ContainsAny(arr[i], "-c & -m & -a & -s") {
+			return i - 1
+		}
+	}
+	return len(arr)-1
+}
+
+func check_opt() Opt {
+	var opts Opt
+	opts.opt_A[0] = 0
+	opts.opt_M[0] = 0
+	opts.opt_S[0] = 0
+	opts.opt_C[0] = 0
+	for i := 0; i < len(os.Args); i++ {
+		if os.Args[i] == "-a" {
+			opts.opt_A[0] = i + 1
+			opts.opt_A[1] = endpoint(i+2, os.Args)
+		} else if os.Args[i] == "-m" {
+			opts.opt_M[0] = i + 1
+			opts.opt_M[1] = endpoint(i+2, os.Args)
+		} else if os.Args[i] == "-c" {
+			opts.opt_C[0] = i + 1
+			opts.opt_C[1] = endpoint(i+2, os.Args)
+		} else if os.Args[i] == "-s" {
+			opts.opt_S[0] = i + 1
+			opts.opt_S[1] = endpoint(i+2, os.Args)
+		}
+	}
+	return opts
+}
 
 func Make_Config(id string, passwd string) *ssh.ClientConfig {
 	config := &ssh.ClientConfig{
@@ -22,38 +70,35 @@ func Make_Config(id string, passwd string) *ssh.ClientConfig {
 	return config
 }
 
-type Opt struct {
-	opt_A int
-	opt_M int
-	opt_C int
-	opt_S int
-}
-
-func check_opt() Opt {
-	var opts Opt
-	opts.opt_A = 0
-	opts.opt_M = 0
-	opts.opt_S = 0
-	opts.opt_C = 0
-	for i := 0; i < len(os.Args); i++ {
-		if os.Args[i] == "-a" {
-			opts.opt_A = i + 1
-		} else if os.Args[i] == "-m" {
-			opts.opt_M = i + 1
-		} else if os.Args[i] == "-c" {
-			opts.opt_C = i + 1
-		} else if os.Args[i] == "-s" {
-			opts.opt_S = i + 1
-		}
+func cmd_run(client *ssh.Client, cmd string) {
+	session, err := client.NewSession()
+	defer session.Close()
+	if err != nil {
+		log.Fatal("Faile to create session : ", err)
 	}
-	return opts
+	if err := session.Run(cmd); err != nil {
+		log.Fatal("Failed to run "+cmd, err)
+	}
 }
-
-type Config struct {
-	Addr   string
-	Role   string
-	Port   string
-	Passwd string
+func copy_run(start_index, end_index int, role_wg *sync.WaitGroup, client *ssh.Client) {
+	defer role_wg.Done()
+	var cp_wg sync.WaitGroup
+	for j := start_index; j < end_index; j++ {
+		cp_wg.Add(1)
+		go func(filePath, desPath string, client *ssh.Client) {
+			session, err := client.NewSession()
+			defer session.Close()
+			if err != nil {
+				log.Fatal("Failed to create session: ", err)
+			}
+			defer cp_wg.Done()
+			err = scp.CopyPath(filePath, desPath, session)
+			if err != nil {
+				log.Fatal("Failed to scp: " + err.Error())
+			}
+		}(os.Args[j], os.Args[end_index], client)
+	}
+	cp_wg.Wait()
 }
 
 func main() {
@@ -71,74 +116,39 @@ func main() {
 		go func(ip, port, passwd, role string) {
 			defer wg.Done()
 			addr := ip + ":" + port
-			config := Make_Config("root", passwd)
+			config := Make_Config("vagrant", passwd)
 			client, err := ssh.Dial("tcp", addr, config)
 			if err != nil {
 				log.Fatal("Failed to dial: ", err)
 			}
 			if opt == "-sh" {
-				if check.opt_C != 0 {
-					session, err := client.NewSession()
-					defer session.Close()
-					if err != nil {
-						log.Fatal("Failed to create session: ", err)
-					}
-					cmd := os.Args[check.opt_C]
-					if err := session.Run(cmd); err != nil {
-						log.Fatal("Failed to run: " + err.Error())
-					}
+				if check.opt_C[0] != 0 {
+					cmd_run(client, os.Args[check.opt_C[0]])
 				}
-				if (check.opt_A != 0) && (role == "agent") {
-					session, err := client.NewSession()
-					if err != nil {
-						log.Fatal("Failed to create session: ", err)
-					}
-					cmd := os.Args[check.opt_A]
-					if err := session.Run(cmd); err != nil {
-						log.Fatal("Failed to run " + err.Error())
-					}
-				} else if (check.opt_M != 0) && (role == "master") {
-					session, err := client.NewSession()
-					if err != nil {
-						log.Fatal("Failed to create session: ", err)
-					}
-					cmd := os.Args[check.opt_M]
-					if err := session.Run(cmd); err != nil {
-						log.Fatal("Failed to run " + err.Error())
-					}
-				} else if (check.opt_S != 0) && (role == "storage") {
-					session, err := client.NewSession()
-					if err != nil {
-						log.Fatal("Failed to create session: ", err)
-					}
-					cmd := os.Args[check.opt_S]
-					if err := session.Run(cmd); err != nil {
-						log.Fatal("Failed to run " + err.Error())
-					}
+				if (check.opt_A[0] != 0) && (role == "agent") {
+					cmd_run(client, os.Args[check.opt_A[0]])
+				} else if (check.opt_M[0] != 0) && (role == "master") {
+					cmd_run(client, os.Args[check.opt_M[0]])
+				} else if (check.opt_S[0] != 0) && (role == "storage") {
+					cmd_run(client, os.Args[check.opt_S[0]])
 				}
 			} else if opt == "-cp" {
-				if (check.opt_A | check.opt_M |check.opt_C | check.opt_S) != 0 {
-					fmt.Println("'-a' or '-m' can be used for '-sh' option")
-					return
+				var role_wg sync.WaitGroup
+				if check.opt_C[0] != 0 {
+					role_wg.Add(1)
+					go copy_run(check.opt_C[0], check.opt_C[1], &role_wg, client)
 				}
-				desPath := os.Args[len(os.Args)-1]
-				var cp_wg sync.WaitGroup
-				for j := 3; j < len(os.Args)-1; j++ {
-					cp_wg.Add(1)
-					go func(filePath, desPath string, client *ssh.Client) {
-						session, err := client.NewSession()
-						defer session.Close()
-						if err != nil {
-							log.Fatal("Failed to create session: ", err)
-						}
-						defer cp_wg.Done()
-						err = scp.CopyPath(filePath, desPath, session)
-						if err != nil {
-							log.Fatal("Failed to scp: " + err.Error())
-						}
-					}(os.Args[j], desPath, client)
+				if (check.opt_A[0] != 0) && (role == "agent") {
+					role_wg.Add(1)
+					go copy_run(check.opt_A[0], check.opt_A[1], &role_wg, client)
+				} else if (check.opt_M[0] != 0) && (role == "master") {
+					role_wg.Add(1)
+					go copy_run(check.opt_M[0], check.opt_M[1], &role_wg, client)
+				} else if (check.opt_S[0] != 0) && (role == "storage") {
+					role_wg.Add(1)
+					go copy_run(check.opt_S[0], check.opt_S[1], &role_wg, client)
 				}
-				cp_wg.Wait()
+				role_wg.Wait()
 			}
 		}(data[i].Addr, data[i].Port, data[i].Passwd, data[i].Role)
 	}
